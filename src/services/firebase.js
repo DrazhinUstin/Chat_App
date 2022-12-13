df288ea9db3d8c23dd683d370d6474dd5ff867c7
DrazhinUstin <zmagar91@gmail.com>
2022-12-14 02:18:40 +0300

    Initial commit

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: 'chat-d9d8c.firebaseapp.com',
+    projectId: 'chat-d9d8c',
+    storageBucket: 'chat-d9d8c.appspot.com',
+    messagingSenderId: '112037357500',
+    appId: '1:112037357500:web:3ade32125468db79efedb9',
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
